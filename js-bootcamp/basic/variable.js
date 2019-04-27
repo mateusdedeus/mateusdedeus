973937fc2eb43708b60b96c442bccf39e219d4ea
mateusdedeus <contato@mateusdedeus.com.br>
2019-04-27 16:42:33 -0300

    básico de js

---
+++ js-bootcamp/basic/variable.js
@@ -0,0 +1,16 @@
+// 1. Não pode definir a variavel + de uma vez
+let petName = 'Hal'
+petName = 'Spot'
+console.log(petName)
+
+//2 Existem regras relacionadas a nomes de variaveis
+
+let test_ = 2
+let result = 3 + 4 
+
+// 3 Variavel pode começar com letra maiuscula ou minuscula de A a Z
+//e pode usar somente esses caracteres especiais _ $
+// Nunca usar nomes reservados para uma variavel ex: let
+
+//let let = 12 
+
